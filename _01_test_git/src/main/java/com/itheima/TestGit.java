a99339df89f54545bbb48375bb5dbb4cc41aaf62
xiejinyou <1137207800@qq.com>
2019-01-18 20:32:57 +0800

    把本地工程添加到git

---
+++ _01_test_git/src/main/java/com/itheima/TestGit.java
@@ -0,0 +1,11 @@
+package com.itheima;
+
+public class TestGit {
+
+    public static void main(String[] args) {
+
+        System.out.println("git");
+        System.out.println("hello");
+        System.out.println("help");
+    }
+}
